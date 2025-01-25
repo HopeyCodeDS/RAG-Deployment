713b1fd2ca2d4a544f5830b6c85659c09966173c
opeyemimomodu2016@gmail.com <opeyemimomodu2016@gmail.com>
2025-01-25 16:04:33 +0100

    Update BedrockEmbeddings in get_embedding_function.py to include specific model_id and region_name.

--- src/rag_app/get_embedding_function.py
+++ src/rag_app/get_embedding_function.py
@@ -3,6 +3,9 @@ from langchain_aws import BedrockEmbeddings
 
 
 def get_embedding_function():
-    embeddings = BedrockEmbeddings()
+    embeddings = BedrockEmbeddings(
+        model_id="amazon.titan-embed-text-v2:0",
+        region_name="us-east-1"
+    )
     return embeddings
 
