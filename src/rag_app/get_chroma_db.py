ef38ea28530962c8c6388cbf26882dbb5690aaa6
opeyemimomodu2016@gmail.com <opeyemimomodu2016@gmail.com>
2025-01-29 15:17:20 +0100

    Update CHROMA_PATH to point to correct relative location in get_chroma_db.py.

--- src/rag_app/get_chroma_db.py
+++ src/rag_app/get_chroma_db.py
@@ -1,7 +1,7 @@
 from langchain_community.vectorstores import Chroma
 from get_embedding_function import get_embedding_function
 
-CHROMA_PATH = "data/chroma"
+CHROMA_PATH = "../data/chroma"
 CHROMA_DB_INSTANCE = None       # A reference to singleton instance of ChromaDB
 
 
